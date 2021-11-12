d88d154f075153b5021902fa03e81e257a5cd205
User <User@N750JV>
2021-11-12 20:24:06 +0300

    Добавьте файлы проекта.

---
+++ Delegate_lab/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate_lab
+{
+    class Program
+    {
+        #region Задача
+        /* В приложении объявить тип делегата, который ссылается на метод. Требования к сигнатуре метода следующие:
+        * метод получает входным параметром переменную типа double;
+        *метод возвращает значение типа double, которое является результатом вычисления.
+        *
+        *Реализовать вызов методов с помощью делегата, которые получают радиус R и вычисляют:
+        *длину окружности по формуле D = 2 * π* R;
+        *площадь круга по формуле S = π* R²;
+        *объем шара. Формула V = 4/3 * π * R³.
+        *
+        *Методы должны быть объявлены как статические.
+        */
+        #endregion
+        delegate double MyDelegate(double value);
+        static void Main(string[] args)
+        {
+            MyDelegate lengthCircle = LengthCircle;
+            MyDelegate areaCircle = AreaCircle;
+            MyDelegate volumeSphere = VolumeSphere;
+            Console.Write("Введите радиус: ");
+            double r = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Выберите необходимое действие:\n1 Найти длину окружности\n2 Найти площадь круга\n3 Найти объём шара\n4 Выполнить всё\n1 / 2 / 3 / 4: ");
+            int act = Convert.ToInt32(Console.ReadLine());
+            switch (act)
+            {
+                case 1:
+                    Console.WriteLine($"{lengthCircle(r):F2}");
+                    break;
+                case 2:
+                    Console.WriteLine($"{areaCircle(r):F2}");
+                    break;
+                case 3:
+                    Console.WriteLine($"{volumeSphere(r):F2}");
+                    break;
+                case 4:
+                    Console.WriteLine($"Длина окружности {lengthCircle(r):F2}");
+                    Console.WriteLine($"Площадь круга {areaCircle(r):F2}");
+                    Console.WriteLine($"Объём шара {volumeSphere(r):F2}");
+                    break;
+                default:
+                    break;
+            }
+            Console.ReadLine();
+        }
+        static double LengthCircle(double r)
+        {
+            return 2 * Math.PI * r;
+        }
+        static double AreaCircle(double r)
+        {
+            return Math.PI * Math.Pow(r, 2);
+        }
+        static double VolumeSphere(double r)
+        {
+            return (4 * Math.PI * Math.Pow(r, 3)) /3;
+        }
+    }
+}
---
+++ Delegate_lab/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Delegate_lab")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Delegate_lab")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("3e5de16b-7135-4d97-b0d4-d908002be3fe")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
